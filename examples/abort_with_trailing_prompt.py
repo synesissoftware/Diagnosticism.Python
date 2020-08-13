c57cb8d29d51004444ea879436e938f2acc43282
Matt <matthew@synesis.com.au>
2020-08-13 10:16:34 +1000

    + forgot example program examples/abort_with_trailing_prompt.py

---
+++ examples/abort_with_trailing_prompt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import diagnosticism as d
+
+d.set_program_name('ConRep')
+
+d.abort("abort with application-defined TRAILING_PROMPT", trailing_prompt='use --help for help', do_exit=False)
+
+d.abort("default abort", do_exit=False)
+
+d.abort("default abort with default TRAILING_PROMPT", do_exit=False, trailing_prompt=True)
+
+print("setting default trailing prompt")
+d.conrep.set_default_trailing_prompt('use --help to show usage')
+
+d.abort("default abort with default TRAILING_PROMPT", do_exit=False, trailing_prompt=True)
+
+d.abort("default abort", do_exit=False)
+
+
