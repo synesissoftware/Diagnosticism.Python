60d7e84bfbaccfe4ba54a08c5e28e9b63656c226
Matt Wilson <mwilson@broadcastvirtual.com>
2024-08-15 19:30:44 +1000

    + added missing `set_log_filter` export to top-level module

--- diagnosticism/__init__.py
+++ diagnosticism/__init__.py
@@ -12,7 +12,7 @@ __status__      =   'Beta'
 __version__     =   '0.7.0'
 
 from .contingent_reporting import abort, report
-from .logging import enable_logging, is_logging_enabled, log
+from .logging import enable_logging, is_logging_enabled, log, set_log_filter
 from .program_name import *
 from .severity import *
 from .tracing import enable_tracing, is_tracing_enabled, trace
