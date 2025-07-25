c6c0d80c22fddd25eae921ceee690b5800e9f9fa
synesissoftware <matthew@synesis.com.au>
2025-07-25 17:11:53 +1000

    0.12.0 (#13)
    * ignores
    
    * `DOOMScope` now uses `time.perf_counter_ns()` (#11)
    
    * `DOOMScope` now uses `time.perf_counter_ns()`
    
    * CHANGES
    
    * boilerplate
    
    ---------
    
    Co-authored-by: mdwsc <matt@simconverse.com>

--- diagnosticism/__init__.py
+++ diagnosticism/__init__.py
@@ -9,7 +9,7 @@ __email__       =   'matthew@synesis.com.au'
 __license__     =   'BSD-3-Clause'
 __maintainer__  =   'Matt Wilson'
 __status__      =   'Beta'
-__version__     =   '0.12.0'
+__version__     =   '0.12.1'
 
 from .contingent_reporting import (
     abort,
